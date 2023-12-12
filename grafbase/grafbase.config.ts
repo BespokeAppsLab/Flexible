ea2bdcd40b1cea711715cae7c3cc9cf31a72a9bb
Lucas Semenya <thereshi.l@gmail.com>
2023-12-12 15:03:22 +0200

    Refactor Grafbase configuration and GraphQL code

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,42 +1,25 @@
-import { g, config, auth } from '@grafbase/sdk';
+import { g, config, auth } from '@grafbase/sdk'
 
-// @ts-ignore
 const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
+  name: g.string().length({ min: 3, max: 20}),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  description: g.string().length({ max: 1000 }).optional(),
   githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
+  LinkedInUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional(),
-}).auth((rules) => {
-  rules.public().read()
 })
 
-// @ts-ignore
 const Project = g.model('Project', {
-  title: g.string().length({ min: 3 }),
-  description: g.string(), 
-  image: g.url(),
-  liveSiteUrl: g.url(), 
-  githubUrl: g.url(), 
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
-}).auth((rules) => {
-  rules.public().read()
-  rules.private().create().delete().update()
-})
-
-const jwt = auth.JWT({
-  issuer: 'grafbase',
-  secret:  g.env('NEXTAUTH_SECRET')
-})
+  name: g.string().length({ min: 3, max: 20}),
+  description: g.string().length({ max: 1000 }),
+  liveSiteUrl: g.url().optional(),
+  gitHubUrl: g.url().optional(),
+  catergory: g.string().search(),
+  users: g.relation(() => User),
+})  
 
 export default config({
-  schema: g,
-  auth: {
-    providers: [jwt],
-    rules: (rules) => rules.private()
-  },
+  schema: g
 })
-
+  
